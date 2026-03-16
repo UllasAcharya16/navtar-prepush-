178a55b4e1a3db3c1abdd5ffe9a64b7203042901
sumedhnvda <sumedhnavuda@outlook.com>
2026-03-16 19:32:21 +0530

    fix socketprovider wrapper in index.js

--- Navatar-Interface/src/index.js
+++ Navatar-Interface/src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import App from "./App";
-import { SocketProvider } from "./context/SocketProvider";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <SocketProvider>
-        <App />
-      </SocketProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
